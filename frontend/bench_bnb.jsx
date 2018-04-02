1aea20e04056c94869b4afa366256d3cbefe52c4
Yangchen Shen <shyangch@gmail.com>
2018-04-02 10:38:45 -0700

    let state hold current user and errors, finish phase1; update the entry point to Root component
    write actions, reducers and store:
    session_actions.js
    session_reducer.js
    session_errors_reducer.js
    errors_reducer.js
    root_reducer.js
    store.js
    
    edit:
    bench_bnb.jsx
    
    add components:
    app.jsx
    root.jsx
    
    Now state should look like this (if we run "window.getState()" in js console):
    {
      session: {
        currentUser: null,
      },
      errors: {
        session: ["Invalid credentials"]
      }
    }

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
 
 // for testing in js console
+import { signup, login, logout } from './util/session_api_util';
 window.signup = signup;
 window.login = login;
 window.logout = logout;
 
 document.addEventListener('DOMContentLoaded', () => {
+	let store;
+	if (window.currentUser) {
+		const preloadedState = { session: { currentUser: window.currentUser } };
+		store = configureStore(preloadedState);
+		delete window.currentUser;
+	} else {
+		store = configureStore();
+	}
+
+	// just for testing!
+	window.getState = store.getState;
+	window.dispatch = store.dispatch;
+
 	const root = document.getElementById('root');
-	ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+	ReactDOM.render(<Root store={store}/>, root);
 });
